0fd51c3f5f633a674189318c0e879811999d98e1
Joey Biotti <joey@MacBook-Pro-2.local>
2017-10-25 19:55:22 -0500

    program runs

--- app.py
+++ app.py
@@ -1,6 +1,6 @@
 def prime_num():
-    i = int(input("Your number is:\n"))
-    for x in range(2, round(x/2)+ 2):
+    x = int(input("Your number is: "))
+    for i in range(2, round(x/2)+ 2):
         if x % i == 0:
             print("Not a prime number.")
             break
